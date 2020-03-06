5214fedf5871a1fb0c58edea31dd3a06c03960b2
iShi0n <guibs225@gmail.com>
2020-03-06 19:21:48 -0300

    working

---
+++ autolike.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         auto liker
+// @version      1.0
+// @author       Shi0n
+// @match        https://www.instagram.com/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getPosts(){
+        var posts = document.getElementsByClassName("ltpMr Slqrh");
+        var unliked_posts = [];
+
+        for(let i=0; i<posts.length; i++){
+            var svg = posts[i].querySelector("svg");
+
+            if(!posts[i].hasAttribute("liked") && svg.getAttribute("fill") == "#262626"){ // #262626 == unliked post, #ed4956 == liked post
+                unliked_posts.push(posts[i]);
+            }
+        }
+        return unliked_posts;
+    }
+
+    function likePost(post){
+        if(!post.hasAttribute("liked")){
+            var button = post.querySelector("button");
+            button.click();
+            post.setAttribute("liked", true);
+        }
+    }
+    
+    function main(){
+        if(document.location.pathname == "/"){
+            var posts = getPosts();
+            
+            for(let i in posts){
+                likePost(posts[i]);
+            }
+        }
+    }
+
+    setInterval(main, 0.5);
+    
+})();
